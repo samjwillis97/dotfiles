dcbfc68f161ac6952cb375084a56c8ec99a4adb2
Sam Willis <sam@williscloud.org>
2022-08-25 07:53:59 +1000

    Update private_dot_config/nvim/lua/settings.lua

--- private_dot_config/nvim/lua/settings.lua
+++ private_dot_config/nvim/lua/settings.lua
@@ -44,6 +44,8 @@ vim.api.nvim_set_keymap("n", "<C-J>", "<C-W><C-J>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-K>", "<C-W><C-K>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-L>", "<C-W><C-L>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-H>", "<C-W><C-H>", { noremap = true })
+---- NVIMTree
+vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true})
 
 
 -- Chezmoi
