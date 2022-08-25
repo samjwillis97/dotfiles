d148acd1728728598e91a20ca081e569162dcb81
Sam Willis <sam@williscloud.org>
2022-08-25 18:31:16 +1000

    Update private_dot_config/nvim/lua/lsp.lua

--- private_dot_config/nvim/lua/lsp.lua
+++ private_dot_config/nvim/lua/lsp.lua
@@ -37,6 +37,10 @@ require('lspconfig')['tsserver'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
+require('lspconfig')['gopls'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
 require('lspconfig')['rust_analyzer'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
