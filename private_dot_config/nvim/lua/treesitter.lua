bfcc476af4df380fd3bb0bb19e7ae0b7cf2986e6
Sam Willis <sam@williscloud.org>
2022-08-25 16:31:08 +1000

    Update private_dot_config/nvim/lua/treesitter.lua

--- private_dot_config/nvim/lua/treesitter.lua
+++ private_dot_config/nvim/lua/treesitter.lua
@@ -7,21 +7,8 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     disable = {},
   },
-  ensure_installed = {
-    "yaml",
-    "toml",
-    "json",
-    "jsonc",
-    "lua",
-    "typescript",
-    "python",
-    "go",
-    "css",
-    "svelte",
-    "dockerfile",
-    "nix",
-    "c_sharp"
-  }
+  ensure_installed = "all"
+  sync_install = false
 }
 
 
