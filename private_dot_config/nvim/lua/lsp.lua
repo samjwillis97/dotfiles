10159d93cd3dbb19bc68408c09516077dace8314
Sam Willis <sam@williscloud.org>
2022-08-29 11:36:02 +1000

    Update private_dot_config/nvim/lua/lsp.lua

--- private_dot_config/nvim/lua/lsp.lua
+++ private_dot_config/nvim/lua/lsp.lua
@@ -94,6 +94,11 @@ require('lspconfig')['svelte'].setup{
     flags = lsp_flags,
     capabilities = capabilities,
 }
+require('lspconfig')['angularls'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+    capabilities = capabilities,
+}
 require('lspconfig')['rust_analyzer'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
