438aa35e401dfb7471760ee1dc2ec46f714abef7
Sam Willis <sam@williscloud.org>
2022-10-07 18:58:17 +1100

    Update private_dot_config/nvim/lua/format.lua

--- private_dot_config/nvim/lua/format.lua
+++ private_dot_config/nvim/lua/format.lua
@@ -5,18 +5,18 @@ local util = require "formatter.util"
 
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
 require("formatter").setup {
-	filetype = {
-	    go = {
-		function () return {
-		    exe = "gofumpt",
-			args = {
-				},
-			stdin = true,
-		    } 
-		end
-	    },
-	    ["*"] = {
-		require("formatter.filetypes.any").remove_trailing_whitespace
-	    }
-	}
+    filetype = {
+        go = {
+            function () return {
+                exe = "gofumpt",
+                args = {
+                },
+                stdin = true,
+                } 
+            end
+        },
+        ["*"] = {
+            require("formatter.filetypes.any").remove_trailing_whitespace
+        }
+    }
 }
