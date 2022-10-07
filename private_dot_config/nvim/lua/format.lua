3282e033332e30164e42bfcad7d802b79a6ca589
Sam Willis <sam@williscloud.org>
2022-10-07 19:58:46 +1100

    Update private_dot_config/nvim/lua/format.lua

--- private_dot_config/nvim/lua/format.lua
+++ private_dot_config/nvim/lua/format.lua
@@ -2,18 +2,21 @@
 local util = require "formatter.util"
 
 -- Format on Save
+-- augroup FormatAutogroup
+--     autocmd!
+--     autocmd BufWritePost * FormatWrite
+-- augroup END
+local formatGroup = vim.api.nvim_create_augroup("FormatAutogroup", {clear = true})
+vim.api.nvim_create_autocmd("BufWritePost", {
+    command = "FormatWrite",
+    pattern = "*",
+    group = formatGroup,
+})
 
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
 require("formatter").setup {
     filetype = {
         go = {
-            -- gofmt
-            -- function () return {
-            --     exe = "gofmt",
-            --     args = {},
-            --     stdin = true,
-            -- }
-	    -- end
             -- gofumpt
             function () return {
                 exe = "gofumpt",
