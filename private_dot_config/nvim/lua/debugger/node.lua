74a54cba579d161a7b2cd8aa2b22eadb9cf7552d
Sam Willis <sam@williscloud.org>
2022-12-15 13:11:21 +1100

    Update private_dot_config/nvim/lua/debugger/node.lua

--- private_dot_config/nvim/lua/debugger/node.lua
+++ private_dot_config/nvim/lua/debugger/node.lua
@@ -13,17 +13,6 @@ dap.adapters.firefox = {
 
 dap.configurations.typescript = {
 	{
-		name = "Launch",
-		type = "node2",
-		request = "launch",
-		program = "${file}",
-		cwd = vim.fn.getcwd(),
-		sourceMaps = true,
-		protocol = "inspector",
-		console = "integratedTerminal",
-	},
-	{
-		-- For this to work you need to make sure the node process is started with the `--inspect` flag.
 		name = "Attach to process",
 		type = "node2",
 		request = "attach",
