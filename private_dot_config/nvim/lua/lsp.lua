af4743c2717430ff0c259f2767349e126b7fa8aa
Sam Willis <sam@williscloud.org>
2023-01-10 09:14:33 +1100

    Update private_dot_config/nvim/lua/lsp.lua

--- private_dot_config/nvim/lua/lsp.lua
+++ private_dot_config/nvim/lua/lsp.lua
@@ -32,7 +32,7 @@ local on_attach = function(client, bufnr)
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-	-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
 	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
 	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
 	vim.keymap.set("n", "<space>wl", function()
@@ -98,7 +98,7 @@ local lspconfig = require("lspconfig")
 require("mason-lspconfig").setup_handlers({
 	function(server_name)
 		lspconfig[server_name].setup({
-			on_attach = lsp_attach,
+			on_attach = on_attach,
 			flags = lsp_flags,
 			capabilities = capabilities,
 		})
