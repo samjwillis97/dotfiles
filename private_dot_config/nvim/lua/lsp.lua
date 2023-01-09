e50673ea3cfb09b2e4c13fe5e0b0f21c7174cecd
Sam Willis <sam@williscloud.org>
2023-01-10 09:05:38 +1100

    Update private_dot_config/nvim/lua/lsp.lua Update private_dot_config/nvim/lua/settings.lua

--- private_dot_config/nvim/lua/lsp.lua
+++ private_dot_config/nvim/lua/lsp.lua
@@ -74,8 +74,8 @@ cmp.setup({
 		end,
 	},
 	window = {
-		-- completion = cmp.config.window.bordered(),
-		-- documentation = cmp.config.window.bordered(),
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
 		-- C-n, C-p for next and previous in menu
