a0a3d77d72436d153be98ccce5e4d8aa640a0969
Sam Willis <sam@williscloud.org>
2023-01-31 08:27:13 +1100

    Update private_dot_config/nvim/lua/filetree.lua

--- private_dot_config/nvim/lua/filetree.lua
+++ private_dot_config/nvim/lua/filetree.lua
@@ -27,12 +27,17 @@ require("nvim-tree").setup({
 		},
 		-- adaptive_size = true,
 		mappings = {
+			custom_only = false,
 			list = {
-				-- { key = "u", action = "dir_up" },
+				{ key = "l", action = "edit", action_cb = edit_or_open },
+				{ key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
 				{ key = "h", action = "close_node" },
 				{ key = "H", action = "collapse_all", action_cb = collapse_all },
-				{ key = "l", action = "edit", action_cb = edit_or_open},
-				{ key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
+			},
+		},
+		actions = {
+			open_file = {
+				quit_on_open = false,
 			},
 		},
 		width = function()
