d1e8e2390117105e52ba6c46e690a7958fd902e4
Sam Willis <sam@williscloud.org>
2023-01-31 08:21:46 +1100

    Update private_dot_config/nvim/lua/filetree.lua

--- private_dot_config/nvim/lua/filetree.lua
+++ private_dot_config/nvim/lua/filetree.lua
@@ -1,17 +1,48 @@
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.5 -- You can change this too
+
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	sort_by = "case_sensitive",
+	view = {
+		float = {
+			enable = true,
+			open_win_config = function()
+				local screen_w = vim.opt.columns:get()
+				local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+				local window_w = screen_w * WIDTH_RATIO
+				local window_h = screen_h * HEIGHT_RATIO
+				local window_w_int = math.floor(window_w)
+				local window_h_int = math.floor(window_h)
+				local center_x = (screen_w - window_w) / 2
+				local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+				return {
+					border = "rounded",
+					relative = "editor",
+					row = center_y,
+					col = center_x,
+					width = window_w_int,
+					height = window_h_int,
+				}
+			end,
+		},
+		-- adaptive_size = true,
+		mappings = {
+			list = {
+				-- { key = "u", action = "dir_up" },
+				{ key = "h", action = "close_node" },
+				{ key = "H", action = "collapse_all", action_cb = collapse_all },
+				{ key = "l", action = "edit", action_cb = edit_or_open},
+				{ key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
+			},
+		},
+		width = function()
+			return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+		end,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
 })
