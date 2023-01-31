f87359b11fce0c3e8df5209537793f1bb2befe33
Sam Willis <sam@williscloud.org>
2023-01-31 13:00:38 +1100

    Update private_dot_config/nvim/lua/filetree.lua

--- private_dot_config/nvim/lua/filetree.lua
+++ private_dot_config/nvim/lua/filetree.lua
@@ -1,52 +1,3 @@
-local lib = require("nvim-tree.lib")
-local view = require("nvim-tree.view")
-
-
-local function collapse_all()
-    require("nvim-tree.actions.tree-modifiers.collapse-all").fn()
-end
-
-local function edit_or_open()
-    -- open as vsplit on current node
-    local action = "edit"
-    local node = lib.get_node_at_cursor()
-
-    -- Just copy what's done normally with vsplit
-    if node.link_to and not node.nodes then
-        require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
-        view.close() -- Close the tree if file was opened
-
-    elseif node.nodes ~= nil then
-        lib.expand_or_collapse(node)
-
-    else
-        require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
-        view.close() -- Close the tree if file was opened
-    end
-
-end
-
-local function vsplit_preview()
-    -- open as vsplit on current node
-    local action = "vsplit"
-    local node = lib.get_node_at_cursor()
-
-    -- Just copy what's done normally with vsplit
-    if node.link_to and not node.nodes then
-        require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
-
-    elseif node.nodes ~= nil then
-        lib.expand_or_collapse(node)
-
-    else
-        require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
-
-    end
-
-    -- Finally refocus on tree if it was lost
-    view.focus()
-end
-
 local HEIGHT_RATIO = 0.8 -- You can change this
 local WIDTH_RATIO = 0.5 -- You can change this too
 
@@ -74,24 +25,9 @@ require("nvim-tree").setup({
 				}
 			end,
 		},
-		-- adaptive_size = true,
-		mappings = {
-			custom_only = false,
-			list = {
-				{ key = "l", action = "edit", action_cb = edit_or_open },
-				{ key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
-				{ key = "h", action = "close_node" },
-				{ key = "H", action = "collapse_all", action_cb = collapse_all },
-			},
-		},
 		width = function()
 			return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
 		end,
-	},
-	actions = {
-		open_file = {
-			quit_on_open = false,
-		},
 	},
 	renderer = {
 		group_empty = true,
